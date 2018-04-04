b21caa8c85d9e9145aa66b9a67f91d9962a5e9e8
Molnár Márton <molnar.marton@gmail.com>
2018-04-04 19:19:45 +0200

    térképes megjelenítés (egyelőre semmi extra)

---
+++ szbz-utvonal/terkep.js
@@ -0,0 +1,45 @@
+﻿var map = L.map('terkep').setView([47.3025, 17.769], 9);
+
+setupLayers(map);
+
+function setupLayers(map) {
+    var roadmap = new L.gridLayer.googleMutant({ type: 'roadmap' }),
+        satellite = new L.gridLayer.googleMutant({ type: 'satellite' }),
+        hybrid = new L.gridLayer.googleMutant({ type: 'hybrid' }),
+        terrain = L.gridLayer.googleMutant({ type: 'terrain' });
+
+    var baseMaps = {
+        'Térkép': roadmap,
+        'Műhold': satellite,
+        'Hibrid': hybrid,
+        'Domborzat': terrain
+    };
+
+    var turaterkep = L.tileLayer('//{s}.map.turistautak.hu/tiles/lines/{z}/{x}/{y}.png', {
+        attribution: '<a href="http://turistautak.hu">turistautak.hu</a>',
+        minZoom: 8,
+        maxZoom: 21,
+        subdomains: 'abcd'
+    });
+
+    var layerControl = new L.Control.Layers(baseMaps, { 'Turistautak': turaterkep }, { collapsed: false });
+    map.addControl(layerControl);
+
+    map.addLayer(roadmap);
+
+    addUtvonal(layerControl);
+}
+
+function addUtvonal(layerControl) {
+    var files = ["Győr-Pannonhalma.gpx", "Pannonhalma-Bakonyszentlászló.gpx", "Bakonyszentlászló-Bakonybél.gpx", "Bakonybél-Veszprém.gpx", "Veszprém-Tótvázsony.gpx", "Tótvázsony-Tihany.gpx"];
+    var layers = [];
+
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        layers.push(new L.GPX('/gpx/' + file, { async: true }));
+    }
+
+    var layerGroup = L.layerGroup(layers);
+    layerControl.addOverlay(layerGroup, 'Útvonal');
+    map.addLayer(layerGroup);
+}
